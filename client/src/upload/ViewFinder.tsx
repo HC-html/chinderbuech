e419682a7b5b958e355b0ebad12b1792893a9ae9
Livio Brunnner <livio.brunner.lb1@gmail.com>
2019-11-16 06:44:30 +0100

    Kamera & so

--- client/src/upload/ViewFinder.tsx
+++ client/src/upload/ViewFinder.tsx
@@ -1,60 +1,59 @@
-import React, { Component } from 'react';
-import 'react-html5-camera-photo/build/css/index.css';
-import axios from 'axios'
-import { AXIOS_CONFIG } from '../constants';
-import styled from 'styled-components';
+import React from "react";
+import "react-html5-camera-photo/build/css/index.css";
+import axios from "axios";
+import { AXIOS_CONFIG } from "../constants";
+import styled from "styled-components";
 
 let cam = require("react-html5-camera-photo");
-let Camera = cam.Camera;
-let FACING_MODES = cam.FACING_MODES;
-let IMAGE_TYPES = cam.IMAGE_TYPES;
+
 const CameraDiv = styled.div`
-   /* width: 100px;
-   position: absolute;
-   bottom: 0px; */
-   position: relative;
-`
+  position: relative;
+`;
 
-class ViewFinder extends Component {
-   onTakePhoto(dataUri: any) {
-      let file = this.dataURLtoFile(dataUri, 'file.jpg');
-      let fd = new FormData();
-      fd.append('file', file);
-      let customConfig = JSON.parse(JSON.stringify(AXIOS_CONFIG))
-      customConfig.headers['Content-Type'] = 'multipart/form-data';
-      axios.post('posts/image', fd, customConfig);
-   }
-   dataURLtoFile(dataurl: string, filename: string) {
-      let arr: any = dataurl.split(','),
-         mime = arr[0].match(/:(.*?);/)[1],
-         bstr = atob(arr[1]),
-         n = bstr.length,
-         u8arr = new Uint8Array(n);
-      while (n--) {
-         u8arr[n] = bstr.charCodeAt(n);
-      }
-      return new File([u8arr], filename, { type: mime });
-   };
-   render() {
-      return (
-         <CameraDiv>
-            <Camera
-               onTakePhoto={(dataUri: any) => { this.onTakePhoto(dataUri); }}
-               idealFacingMode={FACING_MODES.USER}
-               imageType={IMAGE_TYPES.JPG}
-               // idealResolution={{ width, height }}
-               imageCompression={0.97}
-               isMaxResolution={false}
-               isImageMirror={false}
-               isSilentMode={false}
-               isDisplayStartCameraError={true}
-               isFullscreen={false}
-               sizeFactor={1}
-            />
-         </CameraDiv>
-      );
-   }
-}
+const ViewFinder: React.FC = () => {
+  const Camera = cam.Camera;
+  const FACING_MODES = cam.FACING_MODES;
+  const IMAGE_TYPES = cam.IMAGE_TYPES;
 
+  const onTakePhoto = (dataUri: any) => {
+    let file = dataURLtoFile(dataUri, "file.jpg");
+    let fd = new FormData();
+    fd.append("file", file);
+    let customConfig = { ...AXIOS_CONFIG } as any;
+    customConfig.headers["Content-Type"] = "multipart/form-data";
+    axios.post("posts/image", fd, customConfig).then(() => {
+      alert("Bild hochgeladen!");
+    });
+  };
 
-export default ViewFinder
+  const dataURLtoFile = (dataurl: string, filename: string) => {
+    let arr: any = dataurl.split(","),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, { type: mime });
+  };
+
+  return (
+    <CameraDiv>
+      <Camera
+        onTakePhoto={onTakePhoto}
+        idealFacingMode={FACING_MODES.USER}
+        imageType={IMAGE_TYPES.JPG}
+        // idealResolution={{ width, height }}
+        imageCompression={0.97}
+        isMaxResolution={false}
+        isImageMirror={false}
+        isSilentMode={false}
+        isDisplayStartCameraError={true}
+        isFullscreen={false}
+        sizeFactor={1}
+      />
+    </CameraDiv>
+  );
+};
+export default ViewFinder;
